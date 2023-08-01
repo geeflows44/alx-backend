5cfed95de696862aed79d52a54a9454a3d0c1917
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2023-08-01 22:42:37 +0000

    new file

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
-"""the babel instance module"""
-
+""" Parametrize templates
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel
-from flask import Flask, render_template, request
-
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config:
-    """configuration class for babel"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    """ Config
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
@@ -21,18 +25,17 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """Use request.accept_languages to determine the best
-    match with our supported languages"""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-@app.route('/', methods=["GET"], strict_slashes=False)
-def hello():
+    """ determine the best match with our supported languages
     """
-    hello.
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ return a hello world page
     """
     return render_template('3-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
